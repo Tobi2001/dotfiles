b41fd53085ec3722b0b0d8689c5b8ee5f0579c59
Tobias Allgeyer <t.allgeyer@denso-adas.de>
2019-07-24 08:07:30 +0200

    Add useful ros2 scripts

---
+++ ros2_scripts/ros-tmux.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+tmux new-session -d -s ros2
+tmux rename-window 'build'
+tmux send-keys 'roscd' 'C-m'
+tmux send-keys 'clear' 'C-m'
+
+tmux new-window -n 'run'
+tmux send-keys 'roscd' 'C-m'
+tmux send-keys 'clear' 'C-m'
+tmux split-window -h
+tmux send-keys 'roscd' 'C-m'
+tmux send-keys 'htop' 'C-m'
+tmux split-window -v
+tmux send-keys 'roscd' 'C-m'
+tmux send-keys 'clear' 'C-m'
+tmux select-pane -t 0
+tmux split-window -v
+tmux send-keys 'roscd' 'C-m'
+tmux send-keys 'clear' 'C-m'
+tmux select-pane -t 0
+
+tmux new-window -n 'files'
+tmux send-keys 'roscd' 'C-m'
+tmux send-keys 'clear' 'C-m'
+tmux select-window -t 0
+tmux attach-session -t ros2
